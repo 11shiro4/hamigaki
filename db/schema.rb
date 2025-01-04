756a038720a741df596af8bf405ea3f3c0ada697
11shiro4 <yumako345@gmail.com>
2025-01-04 21:25:43 +0900

    エラーメッセージの翻訳

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_04_055501) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_04_122258) do
   create_table "bacteria", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -28,10 +28,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_04_055501) do
   create_table "kids", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.integer "age", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
     t.index ["email"], name: "index_kids_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_kids_on_reset_password_token", unique: true
   end
 
   create_table "kids_bacteria", charset: "utf8mb3", force: :cascade do |t|
