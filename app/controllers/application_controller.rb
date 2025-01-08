849bacb4d346f477ae9846b30bc65a7a60d12782
11shiro4 <yumako345@gmail.com>
2025-01-08 16:08:40 +0900

    ベーシック認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
 
@@ -10,4 +11,12 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "2222"
+    end
+  end
 end
